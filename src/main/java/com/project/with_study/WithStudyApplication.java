2cc83e27a850af8183cfbd76aa8052b827b1048b
chaewonJo <ch730268@gmail.com>
2026-05-06 17:49:07 +0900

    Project Initialize

---
+++ src/main/java/com/project/with_study/WithStudyApplication.java
@@ -0,0 +1,13 @@
+package com.project.with_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithStudyApplication.class, args);
+	}
+
+}
