94ceb88c4b174f8c9683946536c17512b3ded15a
chaewonJo <ch730268@gmail.com>
2026-05-09 15:16:52 +0900

    Feat: BaseEntity 구현

---
+++ src/main/java/com/project/with_study/global/entity/BaseAuthEntity.java
@@ -0,0 +1,24 @@
+package com.project.with_study.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(value = AuditingEntityListener.class)
+public abstract class BaseAuthEntity extends BaseEntity{
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(nullable = false)
+    private String lastModifiedBy;
+
+}
