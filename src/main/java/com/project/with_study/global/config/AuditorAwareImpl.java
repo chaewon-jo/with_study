b426ec09ffd0d65e296fa5739f970fbbcf429588
chaewonJo <ch730268@gmail.com>
2026-05-09 15:44:11 +0900

    Feat: JpaAuditConfig, AuditorAwareImpl 구현 (@EnableJpaAuditing 관련 설정 파일)

---
+++ src/main/java/com/project/with_study/global/config/AuditorAwareImpl.java
@@ -0,0 +1,23 @@
+package com.project.with_study.global.config;
+
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        // Spring Security 컨텍스트에서 인증 정보를 가져오기
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(authentication.getName());
+    }
+}
---
+++ src/main/java/com/project/with_study/global/config/JpaAuditConfig.java
@@ -0,0 +1,14 @@
+package com.project.with_study.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditConfig {
+
+    public AuditorAware<String> auditorAware(){
+        return new AuditorAwareImpl();
+    }
+}
