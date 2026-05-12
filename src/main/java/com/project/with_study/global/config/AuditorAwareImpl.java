e089fb8f62e57f8978c752d1c71d6d268bc7f705
chaewonJo <ch730268@gmail.com>
2026-05-12 19:34:12 +0900

    Fix: 코드 리뷰 반영(AuditorAwareImpl, 익명 사용자 배제)

--- src/main/java/com/project/with_study/global/config/AuditorAwareImpl.java
+++ src/main/java/com/project/with_study/global/config/AuditorAwareImpl.java
@@ -2,6 +2,7 @@ package com.project.with_study.global.config;
 
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -14,7 +15,10 @@ public class AuditorAwareImpl implements AuditorAware<String> {
         // Spring Security 컨텍스트에서 인증 정보를 가져오기
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        if (authentication == null || !authentication.isAuthenticated()) {
+        // 익명사용자 배제
+        if (authentication == null
+                || !authentication.isAuthenticated()
+                || authentication instanceof AnonymousAuthenticationToken) {
             return Optional.empty();
         }
 
