570d9d6a6bfd01fb6ddadc0847809908ddbd6a4f
chaewonJo <ch730268@gmail.com>
2026-05-13 17:50:53 +0900

    Fix: MemberErrorCode의 PASSWORD_MISMATCH 오타 수정

--- src/main/java/com/project/with_study/domain/member/exception/errorcode/MemberErrorCode.java
+++ src/main/java/com/project/with_study/domain/member/exception/errorcode/MemberErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum MemberErrorCode implements ErrorCode {
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "M001", "이미 존재하는 이메일입니다.", "다른 이메일을 입력해주세요."),
-    PASSWORD_MISSMATCH(HttpStatus.BAD_REQUEST, "M002", "비밀번호가 불일치합니다.", "확인 비밀번호를 다시 확인해주세요."),
+    PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "M002", "비밀번호가 불일치합니다.", "확인 비밀번호를 다시 확인해주세요."),
     LOGIN_MISMATCH(HttpStatus.BAD_REQUEST, "M003", "아이디 혹은 비밀번호가 틀렸습니다.", "아이디 혹은 비밀번호를 재입력해주세요.");
 
     private final HttpStatus httpStatus;
