729e027ad0200e3a000130a1cd666e89d740d9f2
Chaewon Jo <notioncwj@gmail.com>
2026-05-15 17:42:08 +0900

    Fix: Member 엔티티에 Bean Validation 추가

--- src/main/java/com/project/with_study/domain/member/entity/Member.java
+++ src/main/java/com/project/with_study/domain/member/entity/Member.java
@@ -3,6 +3,10 @@ package com.project.with_study.domain.member.entity;
 import com.project.with_study.domain.participant.entity.Participant;
 import com.project.with_study.global.entity.BaseEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.SQLDelete;
@@ -25,22 +29,30 @@ public class Member extends BaseEntity {
     private Long id;
 
     @Comment("회원 이름")
+    @NotBlank
+    @Size(min = 2, max = 8)
     @Column(length = 8, nullable = false)
     private String name;
 
     @Comment("닉네임")
+    @NotBlank
+    @Size(max = 16)
     @Column(length = 16, nullable = false)
     private String nickname;
 
     @Comment("비밀번호")
+    @NotBlank
     @Column(length = 60, nullable = false)
     private String password;
 
     @Comment("이메일")
+    @NotBlank
+    @Email
     @Column(nullable = false, unique = true)
     private String email;
 
     @Comment("전화번호")
+    @NotBlank
     @Column(nullable = false, unique = true)
     private String phoneNumber;
 
@@ -51,7 +63,9 @@ public class Member extends BaseEntity {
     @Comment("회원 권한")
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private Authority role = Authority.MEMBER;
+    @NotNull
+    @Builder.Default
+    private Authority authority = Authority.MEMBER;
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
